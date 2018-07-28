7c1e943469c3bd05b5fc595832a8a26daf0232ca
JokerShao <zexishao@foxmail.com>
2018-07-28 15:44:45 +0800

    fixed spell error

--- update/Update_hosts.py
+++ update/Update_hosts.py
@@ -29,7 +29,7 @@ def get_hosts():
         raw_hosts = urllib2.urlopen(url).read()
     except urllib2.URLError:
             print "Network connect failed!"
-            raise NerwrokError
+            raise NetwrokError
     else:
         return raw_hosts, raw_hosts[103:113]
 
